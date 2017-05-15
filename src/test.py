80ca56fe1a8770a476aaec4cae3122adbbb14fb6
Fabien Dobat <fabien.dobat@seniormedia.fr>
2017-05-15 17:14:25 +0200

    Fix tests

--- src/test.py
+++ src/test.py
@@ -4,53 +4,73 @@ from linucb import LinUCB
 ucb = LinUCB(storage = False)
 
 # Assert that rewarding only arm 1 and not 0 set the best arm to 1
+ucb.store.create(['a', 'b', 'c'])
+
 for i in range(1000):
   # Set random scores
-  x = np.random.random((ucb.n_arms, ucb.n_features))
+  x = dict()
+  for n in ucb.store.theta:
+    x[n] = np.random.random(ucb.store.n_features())
 
   data = ucb.get_arm(x)
-  arm_x = data['x']
-  arm_n = int(data['arm'])
 
-  if (arm_n == 1):
+  arm_x = data['x']
+  arm_n = data['arm']
+
+  if (arm_n == 'a'):
     reward = 100
   else:
     reward = 0
 
-  ucb.reward(arm_x, arm_n, reward)
+  X = list()
+  for i in arm_x:
+    X.append(float(i))
+  X = np.asarray(X)
 
-assert (arm_n == 1)
+  ucb.reward(X, arm_n, reward)
 
+assert (arm_n == 'a')
 
 # Reset ucb
 ucb = LinUCB(storage = False)
 
 # Assert that rewarding only if feature 0 is good augmente theta of feature 0
+ucb.store.create(['a', 'b', 'c'])
+
 for i in range(1000):
   # Set random scores
-  x = np.random.random((ucb.n_arms, ucb.n_features))
+  x = dict()
+  for n in ucb.store.theta:
+    x[n] = np.random.random(ucb.store.n_features())
 
   data = ucb.get_arm(x)
   arm_x = data['x']
-  arm_n = int(data['arm'])
+  arm_n = data['arm']
 
   if (arm_x[0] > 0.5):
     reward = 100
   else:
     reward = 0
 
-  ucb.reward(arm_x, arm_n, reward)
+  X = list()
+  for i in arm_x:
+    X.append(float(i))
+  X = np.asarray(X)
 
-for i in range(ucb.n_arms):
-  assert (np.argmax(ucb.store.theta[i]) == 0)
+  ucb.reward(X, arm_n, reward)
+
+for k in ucb.store.theta.keys():
+  assert (np.argmax(ucb.store.theta[k]) == 0)
 
 
 # Reset ucb
 ucb = LinUCB(storage = False)
 
 # Assert that we can create arms
-n_arms = ucb.n_arms
-ucb.create_arms(10)
+n_arms = ucb.store.n_arms()
 
-assert(ucb.n_arms == (n_arms + 10))
-assert(len(ucb.store.theta) == (n_arms + 10))
+arms = ['a', 'b', 'c']
+ucb.store.create(arms)
+
+assert(ucb.store.n_arms() == (n_arms + len(arms)))
+assert(len(ucb.store.theta) == (n_arms + len(arms)))
