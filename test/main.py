70daece5b4a330ec0590f0c0d9d82a3afd88e484
Fabien Dobat <fabien.dobat@seniormedia.fr>
2017-09-27 16:11:42 +0200

    Minor test update

--- test/main.py
+++ test/main.py
@@ -4,24 +4,23 @@ import sys
 sys.path.insert(0, os.getcwd() + '/src')
 
 from linucb import LinUCB
-import arm
-import feature
-import theta
+import arm, feature, theta
 
-# Test methods
-
+# Arms tests
 ucb = LinUCB(storage = False)
 arm.assert_it_gives_best_rewarded_arm(ucb)
 
 ucb = LinUCB(storage = False)
 arm.assert_it_can_create_arms(ucb)
 
+# Features tests
 ucb = LinUCB(storage = False)
 feature.assert_it_increases_theta_of_best_rewarded_feature(ucb)
 
 ucb = LinUCB(storage = False)
 feature.assert_it_can_create_features(ucb)
 
+# Thetas tests
 ucb = LinUCB(storage = False)
 theta.assert_theta_of_each_arm_correspond_to_features(ucb)
 
