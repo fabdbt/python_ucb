d7cb420d8cc7eda045ad5b3d9bcb91e57cdcd167
Fabien Dobat <fabien.dobat@seniormedia.fr>
2020-01-21 09:54:06 +0100

    flake8 linter

--- src/storage.py
+++ src/storage.py
@@ -5,134 +5,137 @@ REQUIRED_STORAGE_FILES = ['theta.npy', 'A.npy', 'b.npy']
 CORRUPTED_MESSAGE = 'Corrupted storage. Please fix it or remove files into folder '
 DEFAULT_N_FEATURES = 1
 
+
 class Storage:
 
-  def __init__(self, persistent = True, path = './storage/'):
-    self.folder = path
-    self.persistent = persistent
+    def __init__(self, persistent=True, path='./storage/'):
+        self.folder = path
+        self.persistent = persistent
 
-    self.A = dict()
-    self.b = dict()
-    self.theta = dict()
+        self.A = dict()
+        self.b = dict()
+        self.theta = dict()
 
-    if self.persistent == True:
-      # Only used at initialization
-      self.files = self.__get_storage_files()
+        if self.persistent:
+            # Only used at initialization
+            self.files = self.__get_storage_files()
 
-      if self.has_storage():
-        self.__check_storage_files()
-        self.load()
-        print('Storage loaded')
+            if self.has_storage():
+                self.__check_storage_files()
+                self.load()
+                print('Storage loaded')
 
-  def has_storage(self):
-    return bool(self.files)
+    def has_storage(self):
+        return bool(self.files)
 
-  def load(self):
-    data = { os.path.splitext(k)[0]: v for k, v in self.files.items() }
+    def load(self):
+        data = {os.path.splitext(k)[0]: v for k, v in self.files.items()}
 
-    if data:
-      self.theta = data['theta']
-      self.A = data['A']
-      self.b = data['b']
+        if data:
+            self.theta = data['theta']
+            self.A = data['A']
+            self.b = data['b']
 
-      return True
-    else:
-      return False
+            return True
+        else:
+            return False
 
-  def create(self, arms = []):
-    for n in arms:
-      if n in self.theta:
-        raise ValueError('Arm(s) already exists')
+    def create(self, arms=[]):
+        for n in arms:
+            if n in self.theta:
+                raise ValueError('Arm(s) already exists')
 
-    if self.n_arms() > 0:
-      n_features = self.n_features()
-    else:
-      n_features = DEFAULT_N_FEATURES # First arm features
+        if self.n_arms() > 0:
+            n_features = self.n_features()
+        else:
+            n_features = DEFAULT_N_FEATURES  # First arm features
 
-    for n in arms:
-      self.A[n] = identity(n_features)
-      self.b[n] = repeat(0.0, n_features)
-      self.theta[n] = repeat(0.0, n_features)
+        for n in arms:
+            self.A[n] = identity(n_features)
+            self.b[n] = repeat(0.0, n_features)
+            self.theta[n] = repeat(0.0, n_features)
 
-    self.save()
+        self.save()
 
-    return arms
+        return arms
 
-  def delete(self, arms = []):
-    for n in arms:
-      if ((n not in self.theta) or (n not in self.A) or (n not in self.b)):
-        raise ValueError(n + ' is not a registered arm')
+    def delete(self, arms=[]):
+        for n in arms:
+            if ((n not in self.theta) or (n not in self.A) or (n not in self.b)):
+                raise ValueError(n + ' is not a registered arm')
 
-    for n in arms:
-      self.theta.pop(n)
-      self.A.pop(n)
-      self.b.pop(n)
+        for n in arms:
+            self.theta.pop(n)
+            self.A.pop(n)
+            self.b.pop(n)
 
-    return self.save()
+        return self.save()
 
-  def add_features(self, total = 1):
-    for t in range(total):
-      for n in self.A:
-        self.A[n] = self.__extend_identity_map(self.A[n])
-        self.b[n] = append(self.b[n], [0.0])
-        self.theta[n] = append(self.theta[n], [0.0])
+    def add_features(self, total=1):
+        for t in range(total):
+            for n in self.A:
+                self.A[n] = self.__extend_identity_map(self.A[n])
+                self.b[n] = append(self.b[n], [0.0])
+                self.theta[n] = append(self.theta[n], [0.0])
 
-    return self.save()
+        return self.save()
 
-  def save(self):
-    if self.persistent == True:
-      self.__save('A', self.A)
-      self.__save('b', self.b)
-      self.__save('theta', self.theta)
+    def save(self):
+        if self.persistent:
+            self.__save('A', self.A)
+            self.__save('b', self.b)
+            self.__save('theta', self.theta)
 
-    return True
+        return True
 
-  def n_arms(self):
-    return len(self.theta)
+    def n_arms(self):
+        return len(self.theta)
 
-  def n_features(self):
-    if self.n_arms() > 0 and len(list(self.A.values())[0]) > 0:
-      return len(list(self.A.values())[0][0])
-    else:
-      return 0
+    def n_features(self):
+        if self.n_arms() > 0 and len(list(self.A.values())[0]) > 0:
+            return len(list(self.A.values())[0][0])
+        else:
+            return 0
 
-  # Private
+    # Private
 
-  def __extend_identity_map(self, identity):
-    copy = identity
+    def __extend_identity_map(self, identity):
+        copy = identity
 
-    copy = append(copy, [[0] * len(copy)], axis=0)
-    copy = append(copy, [[0]] * len(copy), axis=1)
-    copy[len(copy) - 1][len(copy) - 1] = 1
+        copy = append(copy, [[0] * len(copy)], axis=0)
+        copy = append(copy, [[0]] * len(copy), axis=1)
+        copy[len(copy) - 1][len(copy) - 1] = 1
 
-    return copy
+        return copy
 
-  def __check_storage_files(self):
-    # Check all files are presents
-    if (len(self.files) < len(REQUIRED_STORAGE_FILES)):
-      raise Exception(CORRUPTED_MESSAGE + self.folder)
-    else:
-      # Check size of file contents are equals
-      sizes = []
-      for v in self.files.values():
-        sizes.append(len(v))
+    def __check_storage_files(self):
+        # Check all files are presents
+        if (len(self.files) < len(REQUIRED_STORAGE_FILES)):
+            raise Exception(CORRUPTED_MESSAGE + self.folder)
+        else:
+            # Check size of file contents are equals
+            sizes = []
+            for v in self.files.values():
+                sizes.append(len(v))
 
-        if not (all(sizes[0] == size for size in sizes)):
-          raise Exception(CORRUPTED_MESSAGE + self.folder)
+                if not (all(sizes[0] == size for size in sizes)):
+                    raise Exception(CORRUPTED_MESSAGE + self.folder)
 
-  def __get_storage_files(self):
-    files = {}
-    # Prevent from listing directory on each call if files are found
-    for filename in intersect1d(REQUIRED_STORAGE_FILES, os.listdir(self.folder)):
-      files[filename] = self.__get(filename)
+    def __get_storage_files(self):
+        files = {}
+        # Prevent from listing directory on each call if files are found
+        for filename in intersect1d(
+            REQUIRED_STORAGE_FILES, os.listdir(
+                self.folder)):
+            files[filename] = self.__get(filename)
 
-    return files
+        return files
 
-  def __get(self, filename):
-    # np.save method save dict as array
-    # http://stackoverflow.com/a/8361740/4792408
+    def __get(self, filename):
+        # np.save method save dict as array
+        # http://stackoverflow.com/a/8361740/4792408
 
-    return load(self.folder + filename).item()
+        return load(self.folder + filename).item()
 
-  def __save(self, filename, content):
-    return save(self.folder + filename, content)
+    def __save(self, filename, content):
+        return save(self.folder + filename, content)
